2e07bdf3c3472fd983eb8bed6b2f3a959be1bd82
TSeppern <timo.seppern@gmail.com>
2023-03-29 13:00:13 +0000

    123

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
       <BrowserRouter>
         <div className="header">
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/Portfolio/">Home</Link></li>
+            <li><Link to="/Portfolio/contact">Contact</Link></li>
           </ul>
         </div>
         <div className="content">
